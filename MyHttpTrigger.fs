f4ff7f9709403e637485946be5a857b5438dbad0
Jiří Landsman <landsman@cngroup.dk>
2021-03-19 08:20:28 +0100

    wip

--- MyHttpTrigger.fs
+++ MyHttpTrigger.fs
@@ -1,6 +1,7 @@
 namespace functest.Triggers
 open System.Collections.Generic
 open System.Net
+open System.Threading.Tasks
 open Microsoft.Azure.Functions.Worker
 open Microsoft.Azure.Functions.Worker.Http
 open Microsoft.Extensions.Logging
@@ -9,6 +10,10 @@ open Microsoft.Extensions.Logging
 type MyHttpTrigger() =
 
     [<Function("MyHttpTrigger")>]
-    member _.myHttpTrigger
+    member _.MyHttpTrigger
         ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = "myTest")>] req: HttpRequestData, executionContext: FunctionContext) =
-        "hello"
+        let res = req.CreateResponse(HttpStatusCode.OK)
+        res.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        res.WriteString("Welcome to Azure Functions!");
+        res
